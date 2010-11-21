3dc0f111de721858ad7d8bc3b57718fd2d5b5a7c
Pete Muir <pmuir@bleepbleep.org.uk>
2010-11-21 20:34:49 +0000

    Slimline implementations of Annotated*
    * Implementation backed by a set of annotations,
      for use when an extensions has modified the
      metadata
    * Implementation backed by the underlying
      java.lang.reflect object

---
+++ impl/src/main/java/org/jboss/weld/annotated/BaseAnnotated.java
@@ -0,0 +1,22 @@
+package org.jboss.weld.annotated;
+
+import java.lang.reflect.Type;
+
+import javax.enterprise.inject.spi.Annotated;
+
+public abstract class BaseAnnotated implements Annotated
+{
+
+   private final Type baseType;
+   
+   public BaseAnnotated(Type baseType)
+   {
+      this.baseType = baseType;
+   }
+
+   public Type getBaseType()
+   {
+      return baseType;
+   }
+
+}
